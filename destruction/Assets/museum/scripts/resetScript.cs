2bc7ddda70012aa25f9947bbe46dda4645ccebbf
massimo <masst123543@gmail.com>
2026-03-05 23:54:26 -0500

    set reset button

---
+++ destruction/Assets/museum/scripts/resetScript.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class resetScript : MonoBehaviour
+{
+    public static bool playerSpawned = false;
+    
+    public GameObject player;
+    public GameObject playerNoCut;
+
+    static GameObject activePlayer;
+    static string activePlayerName;
+
+    static Vector3 playerPos;
+    static Quaternion playerRot;
+
+
+    void Start()
+    {
+        activePlayer = GameObject.Find(activePlayerName);
+
+        if (!playerSpawned)
+        {
+            //default on scene start
+            activePlayer = player;
+            activePlayerName = player.name;
+            playerPos = player.transform.position;
+            playerRot = player.transform.localRotation;
+
+            setPlayer();
+        }
+        else
+        {
+            setPlayer();
+        }
+
+        //make sure timescale correct cause this can be called from pause
+        Time.timeScale = 1;
+
+        playerSpawned = true;
+    }
+
+    public void resetScene()
+    {
+        //get active player info
+        if (player.active)
+        {
+            activePlayer = player;
+        }
+        else
+        {
+            activePlayer = playerNoCut;
+        }
+        activePlayerName = activePlayer.name;
+        playerPos = activePlayer.transform.position;
+        playerRot = activePlayer.transform.rotation;
+
+        //reset scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void setPlayer()
+    {
+        // activate only active player
+        player.SetActive(false);
+        playerNoCut.SetActive(false);
+
+        activePlayer.SetActive(true);
+
+        //set transform
+        activePlayer.transform.position = playerPos;
+        activePlayer.transform.rotation = playerRot;
+    }
+}
